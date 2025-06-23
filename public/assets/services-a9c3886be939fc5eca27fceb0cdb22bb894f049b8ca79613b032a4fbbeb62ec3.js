c543ffe8b1124a8462f39ff286d1ba6a2aa1bd5b
Clderon <luis.calderon@unas.edu.pe>
2025-06-23 13:06:09 -0500

    normalizando las vistas del frontend PC

--- public/assets/services-a9c3886be939fc5eca27fceb0cdb22bb894f049b8ca79613b032a4fbbeb62ec3.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('turbo:load', function() {
-  // Verificar si estamos en la página correcta antes de ejecutar animaciones
-  if (document.querySelector('.servicios-container') || document.querySelector('.programas-container')) {
-    console.log("En página de servicios o programas - iniciando animaciones");
-    animateCards();
-  }
-});
